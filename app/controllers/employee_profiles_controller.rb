222ef5537021a29521e9f7a01a96610e30f24432
Nicholas Narh <nicknarh@gmail.com>
2016-05-24 21:22:34 -0400

    stuff

--- app/controllers/employee_profiles_controller.rb
+++ app/controllers/employee_profiles_controller.rb
@@ -12,7 +12,7 @@ class EmployeeProfilesController < ApplicationController
 
   def client_authorization
     client = OAuth2::Client.new(Rails.application.secrets.omniauth_provider_key, Rails.application.secrets.omniauth_provider_secret,
-                                :site => Rails.application.secrets.domain_name, :authorize_url => "/api/v1/oauth2/authorize", :token_url => "/api/v1/oauth2/token")
+                                :site => 'https://shielded-brook-62723.herokuapp.com/oauth2/callback', :authorize_url => "/api/v1/oauth2/authorize", :token_url => "/api/v1/oauth2/token")
     client.auth_code.authorize_url(:redirect_uri => 'https://shielded-brook-62723.herokuapp.com/oauth2/callback')
     token = client.auth_code.get_token(session['code'], :redirect_uri => 'https://shielded-brook-62723.herokuapp.com/auth/namely/callback',
                                        :headers => {'Accept' => 'application/json'})
